cb0627db4e56133c37b9e003df836910e2a61f23
Flavio <Flavyoo@users.noreply.github.com>
2021-07-21 03:23:17 +0000

    Remove usage of createMuiTheme

--- src/browser_helpers.tsx
+++ src/browser_helpers.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode } from 'react';
 
 import BaseLayout from './components/layouts/BaseLayout';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 
 // More info on what you can update for the theme here: https://material-ui.com/customization/default-theme/#default-theme
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
       main: `#033FB4`,
